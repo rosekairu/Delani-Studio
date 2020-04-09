3afaee553b52ebeb463a4dc509f218e39c279b7f
Rose Kairu <rosekairu@gmail.com>
2020-04-09 12:39:41 +0300

    add script rule toggle for what we do icons

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".clickable").click(function () {
+    $(".design-hidden").slideToggle();
+    $(".design-showing").slideToggle();
+});
+$(".clickable2").click(function () {
+    $(".development-hidden").slideToggle();
+    $(".development-showing").slideToggle();
+});
+$(".clickable3").click(function () {
+    $(".management-hidden").slideToggle();
+    $(".management-showing").slideToggle();
+});
