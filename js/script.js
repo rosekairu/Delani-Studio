a4e8b3f03ffacb9fde8d7cf1d82c725cf9fa62fe
Rose Kairu <rosekairu@gmail.com>
2020-04-09 22:05:58 +0300

    modify file to make the site responsive

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ $(".clickable3").click(function () {
     $(".management-hidden").slideToggle();
     $(".management-showing").slideToggle();
 });
+$('.imgoverlay').hover(function () {
+    $('.overlaytext', this).slideToggle('slow');
+}, function () {
+    $('.overlaytext', this).slideToggle('slow');
+});
+$("button").click(function (event) {
+    event.preventDefault();
+    let user = document.getElementById('username').value;
+    alert("Dear " + user + ", we have received your message. Thank you for reaching out to us.");
+});
+$("button").on('click', function () {
+    $('form').each(function () {
+        this.reset();
+    });
+});
